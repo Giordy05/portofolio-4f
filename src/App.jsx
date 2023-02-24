e443cb210dacd6c427ef69fc9fa7a8d86d2cff83
Giordy05 <giordano.mandelli1@gmail.com>
2023-02-24 11:59:29 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-
-import './App.css';
-
-function App() {
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import './styles/App.scss';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+}from 'react-router-dom';
+import Contatti from './pages/Contatti';
+import ChiSono from './pages/ChiSono';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+const App = () => {
   return (
-    <div className="App">
+    <Router>
+    <div className='App'>
+      <Navbar />
+      <Routes>
+
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/chi-sono'element= {<ChiSono />} />
+        <Route exact path='/blog' element={<Blog />} />
+        <Route exact path='/contatti' element={<Contatti />} />
       
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/ChiSono.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+ const ChiSono = ( ) =>{
+    return(
+        <h1>CHISONO</h1>
+    )
+ }
+export default ChiSono;
